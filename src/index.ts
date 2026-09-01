928aee2219d2541f1eca42d67ba27d8adb39ea45
Marvin Silverio <silveriomarvin3@gmail.com>
2026-09-01 23:10:09 +0800

    Remove responseContract assignment from index.ts
    Removed the responseContract variable assignment.

--- src/index.ts
+++ src/index.ts
@@ -30,9 +30,6 @@ const ALLOWED_ORIGINS = [
 	"http://127.0.0.1:5173",
 ];
 
-const responseContract =
-	getResponseContract(latestUserMessage);
-
 
 // --------------------------------------------------
 // ASSISTANT PROMPT
